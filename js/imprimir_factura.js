613031256e51fb9537e8b472fecc17cc533c99ef
ezeswci <ezequiel@swci.com.ar>
2016-09-18 15:56:23 -0300

    20160918_1556

--- js/imprimir_factura.js
+++ js/imprimir_factura.js
@@ -5,9 +5,13 @@ function imprimir_factura(factura){
 	var doc = new jsPDF();
 	alert('1');
 	doc.setTextColor(0,0,255);
+	alert('1');
 	doc.text(20, 60, 'Cliente : '+cliente);
+	alert('1');
 	doc.text(20, 50, 'Total Facturado : '+total_factura);
+	alert('1');
 	doc.output('datauri');// lo muestra por pantalla
+	alert('1');
 	//doc.save('comprobante.pdf'); // Lo descarga
 
 }
