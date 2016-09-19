67a31b55921b0b0c566e04e71848c77bba12b632
ezeswci <ezequiel@swci.com.ar>
2016-09-19 14:24:51 -0300

    imprimir

--- js/imprimir_factura.js
+++ js/imprimir_factura.js
@@ -10,16 +10,10 @@ function imprimir_factura()
         var error = function(status) {
             alert('Error: ' + status);}
 		//create(html, filePath, successCallback, errorCallback)
-		var texto = "<html><head></head><body><h1>"+cliente+"</h1><p>Total Factura = "+total_factura+"</p></body></html>"
+		var html = "<html><head></head><body><h1>Prueba de Html</h1><p>Total Factura = $9999</p></body></html>"
 	    //window.html2pdf.create(texto,"~/Download/presupuest.pdf",success,error);
-
-        
-	    window.html2pdf.create(
-            "<html><head></head><body><h1>Prueba 1</h1><p>html content.</p></body></html>",
-            "~/Documents/test.pdf", // on iOS,
-            // "test.pdf", on Android (will be stored in /mnt/sdcard/at.modalog.cordova.plugin.html2pdf/test.pdf)
-            success,
-            error
-        );
+		var nombre="test1.pdf";
+		var filePath=""+nombre;// fileSystem.root+"/swci/" cordova.file.externalApplicationStorageDirectory+"/swci/"
+        window.html2pdf.create(html, filePath, success, error);
 		
 }
