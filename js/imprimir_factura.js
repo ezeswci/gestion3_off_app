4803f18706f6df9afb2c0fd5260c5359b8cdb32d
ezeswci <ezequiel@swci.com.ar>
2016-09-19 13:07:28 -0300

    20160919_1307

--- js/imprimir_factura.js
+++ js/imprimir_factura.js
@@ -1,6 +1,9 @@
 //document.addEventListener('deviceready', onDeviceReady);
-function onDeviceReady()
-{
+function imprimir_factura()
+{		alert(imprimir);
+		var cliente = getCookie('contacto_razon_social');	
+		var total_factura = getCookie('total_factura');
+
         var success = function(status) {
             alert('Message: ' + status);
         }
@@ -8,12 +11,17 @@ function onDeviceReady()
         var error = function(status) {
             alert('Error: ' + status);
         }
+		//create(html, filePath, successCallback, errorCallback)
+		var texto = "<html><head></head><body><h1>"+cliente+"</h1><p>Total Factura = "+total_factura+"</p></body></html>"
+	    window.html2pdf.create(texto,"~/Download/presupuest.pdf",success,error);
 
-        window.html2pdf.create(
+        /*
+	    window.html2pdf.create(
             "<html><head></head><body><h1>Some</h1><p>html content.</p></body></html>",
             "~/Documents/test.pdf", // on iOS,
             // "test.pdf", on Android (will be stored in /mnt/sdcard/at.modalog.cordova.plugin.html2pdf/test.pdf)
             success,
             error
         );
+		*/
 }
