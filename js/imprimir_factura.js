03cf03d6d69c8911f5f05344e6a2857cfcb49f1d
ezeswci <ezequiel@swci.com.ar>
2016-09-19 14:42:58 -0300

    imprimir en vez de PDF

--- js/imprimir_factura.js
+++ js/imprimir_factura.js
@@ -1,19 +1,8 @@
 //document.addEventListener('deviceready', onDeviceReady);
 function imprimir_factura()
-{		alert('imprimir');
-		var cliente = getCookie('contacto_razon_social');	
-		var total_factura = getCookie('total_factura');
-
-        var success = function(status) {
-            alert('Message: ' + status);}
-
-        var error = function(status) {
-            alert('Error: ' + status);}
-		//create(html, filePath, successCallback, errorCallback)
-		var html = "<html><head></head><body><h1>Prueba de Html</h1><p>Total Factura = $9999</p></body></html>"
-	    //window.html2pdf.create(texto,"~/Download/presupuest.pdf",success,error);
-		var nombre="test1.pdf";
-		var filePath=""+nombre;// fileSystem.root+"/swci/" cordova.file.externalApplicationStorageDirectory+"/swci/"
-        window.html2pdf.create(html, filePath, success, error);
-		
+{		alert('imprimir');		
+		var page = '<h1>Hello Document</h1>';
+		cordova.plugins.printer.print(page, 'Document.html', function () {
+			alert('printing finished or canceled')
+		});
 }
